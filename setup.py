9116c3a78241e05155f5483738f7e2485f4d2f8e
David <david@gopreki.com>
2020-07-02 08:58:41 -0500

    Fixed setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name="preki_funcmodels",
-    version="3.0.1",
+    version="1.0.0",
     author="Preki",
     author_email="david@gopreki.com",
     packages=['preki_funcmodels'],
     url='https://gopreki.com',
-    download_url="https://github.com/GoPreki/FunctionUtilsHandler",
+    download_url="https://github.com/GoPreki/FunctionUtilsModels",
     license="MIT",
     description="Python library for Preki Functions usage with Neomodel Models",
     long_description="Python library for Preki Functions usage with Neomodel Models",
